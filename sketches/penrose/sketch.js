8dae829a6c03dff6baaf2ff4ca479f93c3c2cac4
rictorlome <rictorlome@gmail.com>
2018-04-28 13:04:35 -0400

    Render dart

--- sketches/penrose/sketch.js
+++ sketches/penrose/sketch.js
@@ -5,21 +5,31 @@ function setup() {
 function draw() {
   clear()
   background(51)
-  const vec = createVector(10,30)
+  const vec = createVector(20,20)
   const col = color(40,30,30)
-  const point = {x: 200, y: 200}
+  const point = createVector(100,100)
   dart(point,vec,col)
 }
 
 function dart(point,vector,color) {
   fill(color)
   angleMode(DEGREES)
-  const edgeLength = 2 * vector.mag() * cos(36)
-  debugger
-  const wingCoordX1 = point.x + edgeLength * cos(54)
-  const wingCoordX2 = point.x - edgeLength * cos(54)
-  const wingCoordY = point.y + edgeLength * sin(54)
-  triangle(point.x,point.y,wingCoordX1,wingCoordY,point.x+vector.x,point.y+vector.y)
-  triangle(point.x,point.y,wingCoordX2,wingCoordY,point.x+vector.x,point.y+vector.y)
-  // quad(point.x,point.y,wingCoordX1, wingCoordY,point.x+vector.x,point.y+vector.y,wingCoordX2, wingCoordY)
+  const outerWingLength = 2 * vector.mag() * cos(36);
+
+  const x2 = point.x+vector.x;
+  const y2 = point.y+vector.y;
+
+  const rightVec = vector.copy();
+  rightVec.rotate(-72);
+  const leftVec = vector.copy();
+  leftVec.rotate(72);
+
+  const x3 = x2 + rightVec.x
+  const y3 = y2 + rightVec.y
+
+  const x4 = x2 + leftVec.x
+  const y4 = y2 + leftVec.y
+
+  triangle(point.x,point.y,x2,y2,x3,y3)
+  triangle(point.x,point.y,x2,y2,x4,y4)
 }
